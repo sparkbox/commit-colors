7efe7c246e7ca14f6b59faca8703d40bf9dbd8a3
Bryan Braun <bryan@heysparkbox.com>
2018-10-19 11:47:22 -0400

    :bug: fix: variable name typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const chalk = require('chalk');
 const namer = require('color-namer');
-const defaultTempalte = require('./templates/default');
+const defaultTemplate = require('./templates/default');
 
 const hexString = process.argv[2];
 const colorName = namer(hexString).pantone[0].name;
